49c6e289e1f2207c2195e660e1a6df8a9d1a9981
Joseph <joseph.fernando@ti.ukdw.ac.id>
2020-06-07 20:12:08 +0700

    aaaaaaaaaaaa

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php 
     define('ROOT_URL', '/');
-    define('DB_HOST', 'localhost');
+    define('DB_HOST', 'localhost:3306');
     define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_PASS', 'ADSIS-JOSEPH-ARLEN');
     define('DB_NAME', 'website2');
 ?>
 
