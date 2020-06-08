bc99e8a88c766f7fe8f0f4e0f442fe28c53098cf
Joseph <joseph.fernando@ti.ukdw.ac.id>
2020-06-08 21:37:30 +0700

    update dockerfile

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    define('ROOT_URL', '/');
-    define('DB_HOST', '100.25.48.119');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'ADSIS-JOSEPH-ARLEN');
-    define('DB_NAME', 'website2');
-?>
-
