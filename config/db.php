58fe578f0915a94a7e0d98fce985f206921b2cc6
Joseph <joseph.fernando@ti.ukdw.ac.id>
2020-06-08 08:38:49 +0700

    a

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
 //create connection
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
 echo (mysqli_connect_errno())? 'failed to connect to mysql '.mysqli_connect_errno() : '';
 ?>
